16c1cef10619f5553902954ad31d14964cb046d9
“Zahed75” <zahedhasan.cs@gmail.com>
2023-12-16 04:04:31 +0600

    modified:   src/modules/User/model.js

--- src/modules/User/model.js
+++ src/modules/User/model.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const UserSchema= new mongoose.Schema({
   username:{
     type:String,
-    required:[false,'Username Must Be Required'],
     max:[30,'Please Input Your UserName'],
     
   },
